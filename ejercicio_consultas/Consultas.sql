23d32b2a0820eda114118dad5bd59854a2a1a2b5
Juan Velasquez <jsvelsaa@hotmail.com>
2020-09-15 12:07:06 -0500

    finish consultas

--- ejercicio_consultas/Consultas.sql
+++ ejercicio_consultas/Consultas.sql
@@ -1,18 +1,18 @@
-/* Punto 1 */
+/* Lista de todos los datos de la tabla Clientes */
 
 SELECT * FROM Cliente;
 
-/* Punto  2*/
+/* Lista de los datos de los clientes que han realizado ordenes */
 
 SELECT Cliente.*, OrdenVenta.Nro FROM Cliente, OrdenVenta WHERE Cliente.CC == OrdenVenta.CC GROUP BY Cliente.CC;
 
-/* Punto 3*/
+/* Numero y fecha de solicitud de las ordenes de venta quese ahn entregado, con la fecha de entrega */
 
 SELECT OrdenVenta.Nro, OrdenVenta.Fecha_solicitud, Venta.Fecha_Venta
 FROM OrdenVenta, Venta
 WHERE OrdenVenta.Nro == Venta.Nro;
 
-/* Punto 4 */
+/* Lista de empleados que han atendido mas de dos ordenes de venta, ordenados segun la cantidad atendida */
 
 SELECT Empleado.Nombre, Empleado.Apellidos, COUNT(OrdenVenta.Id_Empleado) AS Cantidad_ventas
 FROM Empleado INNER JOIN OrdenVenta
@@ -21,7 +21,7 @@ GROUP BY OrdenVenta.Id_Empleado, Empleado.Apellidos
 HAVING COUNT(OrdenVenta.Id_Empleado) > 2
 ORDER BY Cantidad_ventas DESC;
 
-/* Punto 5 */
+/* Ordenes de venta que no se han realizado hasta la fecha (Nro de orden, cliente que la solicito y empleado que la atendio) */
 
 SELECT OrdenVenta.Nro, Cliente.Nombre AS CN, Cliente.Apellidos AS CA, Empleado.Nombre AS EN, Empleado.Apellidos AS EA
 FROM OrdenVenta LEFT OUTER JOIN Venta ON OrdenVEnta.Nro = Venta.Nro
@@ -29,10 +29,19 @@ INNER JOIN Cliente ON OrdenVenta.CC = Cliente.CC
 INNER JOIN Empleado ON OrdenVenta.Id_Empleado = Empleado.Id
 WHERE Venta.fecha_venta IS NULL;
 
-/* Punto 6 */
+/* Por cada orden de venta el precio total que se debe pagar */
 
-SELECT
+SELECT OrdenVenta_Producto.Nro, SUM((OrdenVenta_Producto.cantidad * Producto.Precio - (OrdenVenta_Producto.cantidad * Producto.Precio * (OrdenVenta_Producto.descuento / 100)))) AS Total_Pago
+FROM OrdenVenta_Producto INNER JOIN Producto
+ON OrdenVenta_Producto.codigo = Producto.Codigo
+Group By OrdenVenta_Producto.Nro;
 
+/* Monto total de las ventas realizadas en el año 2012 */
 
-
-
+SELECT OrdenVenta_Producto.Nro, SUM((OrdenVenta_Producto.cantidad * Producto.Precio - (OrdenVenta_Producto.cantidad * Producto.Precio * (OrdenVenta_Producto.descuento / 100)))) AS Total_Pago
+FROM OrdenVenta_Producto INNER JOIN Producto
+ON OrdenVenta_Producto.codigo = Producto.Codigo
+INNER JOIN OrdenVenta
+ON OrdenVenta_Producto.Nro = OrdenVenta.Nro
+WHERE OrdenVenta.Fecha_solicitud >= '2012-01-01' AND OrdenVenta.Fecha_solicitud < '2013-01-01'
+Group By OrdenVenta_Producto.Nro;
---
+++ ejercicio_subconsultas/SubConsultas.sql
@@ -0,0 +1,25 @@
+--Aquellos productos que tienen el mayor precio.
+
+
+--Órdenes de venta que no se han realizado hasta la fecha (Nro de orden, cliente que --la solicitó y empleado que la atendió)
+--utilizando subconsultas
+
+
+--Aquellas órdenes (cantidad) por empleado que no se cumplieron
+
+
+-- Por cada empleado cantidad de órdenes que no se cumplieron
+-- y tienen más de 15 meses de pedidas.
+
+
+-- Cantidad de órdenes vendidas y no vendidas por cada Empleado (el resultado se
+-- debe mostrar en una solo tabla)
+
+
+--aquellos productos para los que no se han hecho órdenes en el mes actual
+
+
+--Mostrar todos los clientes con la cantidad de órdenes de venta que han realizado
+
+
+--Productos que se han vendido más
